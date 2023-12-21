5d305917b4309a266f74a1e7cb1bfe3695840925
Junaid <junadjamil2304@gmail.com>
2023-12-21 09:24:53 -0800

    Done

--- src/app/Compopnents/logout.js
+++ src/app/Compopnents/logout.js
@@ -4,7 +4,7 @@ import { IoIosLogOut } from "react-icons/io";
 const Logout = () => {
   return (
     <div>
-     <Image src="/Assets/Images/Logout.png" width={192} height={55} alt='Logo' />
+     <Image src="/Assets/Images/logout.png" width={192} height={55} alt='Logo' />
        <button className='mt-10 flex ml-3 items-center justify-between w-full'>LogOut  < IoIosLogOut size={20} className=' mr-10' /></button>   
     </div>
   )
